fea4f5a1dbefd4eab8dacf9d5de4f1d235add2ec
Aaron Zhang <aaronbxzhang@gmail.com>
2025-11-27 23:37:21 -0500

    Add inner enum in EventTypes to represent EventCodes

--- src/EventTypes.java
+++ src/EventTypes.java
@@ -1,20 +1,91 @@
 public enum EventTypes {
-    SYN(0x00),
-    KEY(0x01),
-    REL(0x02),
-    ABS(0x03),
-    MSC(0x04),
-    SW(0x05),
-    LED(0x11),
-    SND(0x12),
-    REP(0x14),
-    FF(0x15),
-    PWR(0x16),
-    FF_STATUS(0x17),
-    MAX(0x1f),
-    CNT(EventTypes.MAX.getEventCode() + 1),
-    NONE(-1);
+    SYN(0x00) {
+        enum EventCodes {
+
+        }
+
+    },
+
+    KEY(0x01) {
+        enum EventCodes {
+            
+        }
+    },
+
+    REL(0x02) {
+        enum EventCodes {
+            
+        }
+    },
+
+    ABS(0x03) {
+        enum EventCodes {
+            
+        }
+    },
+
+    MSC(0x04) {
+        enum EventCodes {
+            
+        }
+    },
+
+    SW(0x05) {
+        enum EventCodes {
+            
+        }
+    },
+
+    LED(0x11) {
+        enum EventCodes {
+            
+        }
+    },
     
+    SND(0x12) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    REP(0x14) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    FF(0x15) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    PWR(0x16) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    FF_STATUS(0x17) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    MAX(0x1f) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    CNT(EventTypes.MAX.getEventCode() + 1) {
+        enum EventCodes {
+            
+        }
+    },
+    
+    NONE(-1);
+
     private int eventTypeValue;
 
     private EventTypes(int eventTypeValue) {
