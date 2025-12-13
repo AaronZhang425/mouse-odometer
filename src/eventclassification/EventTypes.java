3adeeaeb330c60117847b95e127cccb5e0f1a3d7
Aaron Zhang <aaronbxzhang@gmail.com>
2025-12-12 21:49:34 -0500

    Add more mappings to EVENTCODES map in EventTypes enum

--- src/eventclassification/EventTypes.java
+++ src/eventclassification/EventTypes.java
@@ -13,10 +13,10 @@ public enum EventTypes implements EventCategory{
     SW(0x05),
     LED(0x11),
     SND(0x12),
-    REP(0x14),
-    FF(0x15),
-    PWR(0x16),
-    FF_STATUS(0x17);
+    REP(0x14);
+    // FF(0x15),
+    // PWR(0x16),
+    // FF_STATUS(0x17);
 
     // public abstract EventCategory getEventCodeSet();
 
@@ -37,19 +37,84 @@ public enum EventTypes implements EventCategory{
         // methods manually put in to avoid java reflection due to
         // memory overhead
 
-        // Syn
+        // SYN
         EVENTCODES.put(
             EventTypes.SYN, num -> {
                 return Syn.fromValue(num);
             }
         );
 
-        // Key
+        // KEY
         EVENTCODES.put(
             EventTypes.KEY, num -> {
                 return Key.fromValue(num);
             }
         );
+
+        // REL
+        EVENTCODES.put(
+            EventTypes.REL, num -> {
+                return Rel.fromValue(num);
+            }
+        );
+
+        // ABS
+        EVENTCODES.put(
+            EventTypes.ABS, num -> {
+                return Abs.fromValue(num);
+            }
+        );
+
+        // MSC
+        EVENTCODES.put(
+            EventTypes.MSC, num -> {
+                return Msc.fromValue(num);
+            }
+        );
+
+        // SW
+        EVENTCODES.put(
+            EventTypes.SW, num -> {
+                return Sw.fromValue(num);
+            }
+        );
+
+        // LED
+        EVENTCODES.put(
+            EventTypes.LED, num -> {
+                return Led.fromValue(num);
+            }
+        );
+
+        // SND
+        EVENTCODES.put(
+            EventTypes.SND, num -> {
+                return Snd.fromValue(num);
+            }
+        );
+
+        // REP
+        EVENTCODES.put(
+            EventTypes.REP, num -> {
+                return Rep.fromValue(num);
+            }
+        );
+
+        // Could be implemented for wider support
+        // // FF
+        // EVENTCODES.put(
+        //     EventTypes.FF, num -> {
+        //         return FF.fromValue(num);
+        //     }
+        // );
+
+        // // PWR
+        // EVENTCODES.put(
+        //     EventTypes.PWR, num -> {
+        //         return PWR.fromValue(num);
+        //     }
+        // );
+
 
     }
 
