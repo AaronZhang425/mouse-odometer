d521805f50df8a6c8e43257e3e0cba09ce57c322
Aaron Zhang <aaronbxzhang@gmail.com>
2025-12-14 01:01:32 -0500

    Add xValues and yValues arrays of size 2 to Odometer.java

--- src/inputanalysis/Odometer.java
+++ src/inputanalysis/Odometer.java
@@ -1,10 +1,13 @@
 package inputanalysis;
 
+import devicemanagement.EventData;
 import devicemanagement.InputReader;
 
 // to be implemented
 public class Odometer {
     private final InputReader reader;
+    private final EventData[] xValues = new EventData[2];
+    private final EventData[] yValues = new EventData[2];
 
     public Odometer(InputReader reader) {
         this.reader = reader;
