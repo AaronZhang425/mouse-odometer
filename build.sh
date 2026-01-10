2f6c38d5dc943c72b8dc58dfe9143a9b5c6fbe81
Aaron Zhang <aaronbxzhang@gmail.com>
2026-01-09 23:33:38 -0500

    Add shebang to build.sh and run.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 javac -d ./build/ ./src/*.java ./src/inputanalysis/*.java ./src/eventclassification/*.java ./src/eventclassification/eventcodes/*.java ./src/devicemanagement/*.java 
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # bash ./build.sh
 java -cp build Main
